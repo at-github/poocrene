3a113f66b9b7bdc75d21a6415491fbe3a5267c16
Tarik Amar <tarik.amar@gmail.com>
2022-11-17 22:56:41 +0100

    Fix words in tests

--- src/urlParser.test.js
+++ src/urlParser.test.js
@@ -4,23 +4,24 @@ test('Null returned if no id is provided in url', () => {
   expect(extractIdFromUrl('')).toBeNull()
 })
 
-test('Find id in simple url is return', () => {
+test('Find id in one parameter url', () => {
   expect(extractIdFromUrl('/?id=12')).toBe('12')
 })
 
-test('Find id in complex url is return', () => {
+test('Find id in multiple parameters url', () => {
   expect(extractIdFromUrl('/?foo=foo&id=13&bar=bar')).toBe('13')
 })
 
 test('Null return with term close to id', () => {
   expect(extractIdFromUrl('/?foo=foo&pid=14&bar=bar')).toBeNull()
+  expect(extractIdFromUrl('/?foo=foo&ide=14&bar=bar')).toBeNull()
 })
 
-test('Find id near term close to id', () => {
+test('Find id even if a close parameter is like id', () => {
   expect(extractIdFromUrl('/?foo=foo&pid=14&id=15&bar=bar')).toBe('15')
 })
 
-test('Find last id founded', () => {
+test('Last id founded', () => {
   expect(extractIdFromUrl('/?foo=foo&id=16&id=17&bar=bar')).toBe('17')
 })
 
